a437c15d86926514e18cb7373221935e75053eb6
sukanya004sinha <sukanyasinha004@gmail.com>
2025-02-22 17:28:48 +0530

    upto searchbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import {useEffect, useState} from "react"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [input,setInput] = useState("");
+  const [results,setResults] = useState([]);
+  const [showResults  ,setShowResults] = useState(false);
+  const fetchData = async () => {
+      const data = await fetch("https://dummyjson.com/recipes/search?q="+ input);
+      const json = await data.json();
+      setResults(json?.recipes);
+    };
+    useEffect(() => {
+      fetchData();
+    },[input] );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1>Autocomplete Search Bar</h1>
+        <div>
+        <input type = "text"
+         className="search-input" 
+         value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onBlur={() => setShowResults(false)}
+          onFocus={() => setShowResults(true)}
+           />
+           {showResults && (
+           <div className="result-container">
+           {results.map((r) =>
+            <span className= "result" key ={r.id }>{r.name}</span>
+  )}
+
+           </div>
+           )}
+            </div>
+      </div>
+
+
   );
 }
-
-export default App;
