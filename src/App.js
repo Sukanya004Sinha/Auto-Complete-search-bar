599198dcac5717a9fa7d3f27cac4bb3253a83e1f
sukanya004sinha <sukanyasinha004@gmail.com>
2025-02-22 23:55:22 +0530

    upto caching

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import './App.css';
 export default function App() {
   const [input,setInput] = useState("");
   const [results,setResults] = useState([]);
-  const [showResults  ,setShowResults] = useState(false);
+  const [showResults,setShowResults] = useState(false);
+  const [cache,setCache] = useState ({});
+
   const fetchData = async () => {
+    if(cache[input]){
+      console.log("CACHE RETURNED", input);
+      setResults(cache[input]);
+      return;
+    }
+      console.log("API CALL", input);
       const data = await fetch("https://dummyjson.com/recipes/search?q="+ input);
       const json = await data.json();
       setResults(json?.recipes);
-    };
+      setCache(prev => ({
+      ...prev,[input]: json?.recipes
+    }));
+  };
     useEffect(() => {
       const timer = setTimeout(fetchData,300);
       return () => {
         clearTimeout(timer);
       };
     },[input] );
-
+  
   return (
     <div className="App">
         <h1>Autocomplete Search Bar</h1>
